ee150e328c03f01b0f546a2f2503de18324ed99f
spiceyourcode <franklineomari@gmail.com>
2026-08-25 20:32:07 +0300

    Add MySQL2 dependency and implement database connection in server. Introduce test endpoint for database query.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { getAccessToken, generatePassword } from "./services/darajaServices.js";
 import mpesaRoutes from "./routes/mpesaRoutes.js";
+import pool from "./db/db.js"; 
 dotenv.config();
 
 const app = express();
@@ -18,6 +19,19 @@ app.get('/', (req, res)=>{
 })
 
 app.use("/api/mpesa", mpesaRoutes);
+
+app.get("/api/test-db", async(req, res)=>{
+    try{
+        const[rows]= await pool.query("SELECT 1 AS result");
+        res.json({
+            success:true, 
+            data:rows,
+        });
+    }
+    catch(error){
+        console.error(error)
+    }
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
